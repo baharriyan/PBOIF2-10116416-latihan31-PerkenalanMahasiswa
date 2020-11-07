166d49a01bb36576cc5bc5540c01946921fd56c5
baharriyan <indonesiariyan@gmail.com>
2020-11-08 05:42:40 +0700

    first commit

---
+++ PBOIF2-10116416-latihan31-PerkenalanMahasiswa/src/pbo2/pkg10116416/latihan31/perkenalanmahasiswa/mainclass.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10116416.latihan31.perkenalanmahasiswa;
+
+/**
+ *
+ * @author 
+ * NAMA     : riyan bahar
+ * KELAS    : IF-2
+ * NIM      : 10116416
+ */
+public class mainclass {
+    public static void main(String[] args) {
+        Mahasiswa mhs = new Mahasiswa();
+        
+        mhs.nim = "10116416";
+        mhs.nama = "riyan bahar";
+        
+        mhs.perkenalkanDiri();
+        
+        mhs.nim = "10110270";
+        mhs.nama = "Indra Tiola";
+        
+        mhs.perkenalkanDiri();
+        
+        mhs.nim = "10110271";
+        mhs.nama = "Robi Tanzil Ganefi";
+        
+        mhs.perkenalkanDiri();
+        
+        mhs.nim = "10110269";
+        mhs.nama = "Muhammad Nur Awaludin";
+        
+        mhs.perkenalkanDiri();
+        
+
+    }
+    
+}
